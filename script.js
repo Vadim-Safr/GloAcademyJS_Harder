f1c79ce9761244e06baf037e75010cfb352686f7
Vadim Safroshkin <vadimpuskin818@gmail.com>
2026-05-10 15:14:52 +0700

    выполненное усложненное задание lesson03

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-const num = 266219;
-let mult = 1;
-let pow;
+let lang = prompt("Выберите один язык: ru, en", "ru, en");
+const daysWeekRu = "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье";
+const daysWeekEn = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
 
-for (let digit of String(num)) {
-    mult *= digit;
+// if
+if (lang == "ru") {
+    console.log(daysWeekRu);
+} else if (lang == "en") {
+    console.log(daysWeekEn);
+} else {
+    console.log("Что-то пошло не так :(");
 }
 
-console.log(mult);
-pow = mult ** 3;
-console.log(String(pow).slice(0, 2))
+// switch
+switch (lang) {
+    case "ru":
+        console.log(daysWeekRu);
+        break
+    case "en":
+        console.log(daysWeekEn);
+        break
+    default:
+        console.log("Что-то пошло не так :(");
+}
+
+// array 1 способ
+const arr = [
+    ["ru", "Понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
+    ["en", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+];
+
+let findLang = arr.find(element => element[0] == lang);
+
+for (let i = 1; i <= 7; i++) {
+    console.log(findLang[i]);
+}
+
+// array 2 способ
+const arr2 = [
+    ["Понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
+    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+];
+
+const indexLang = {
+    "ru": 0,
+    "en": 1
+};
+
+console.log(arr2[indexLang[lang]].join(", "));
+
+
+const namePerson = prompt("Введите имя", "Александр");
+const role = namePerson == "Артем"
+    ? "директор"
+    : namePerson == "Александр"
+        ? "преподаватель"
+        : "студент";
+
+console.log(role);
